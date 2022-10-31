44a72fda70f9f5c7879af3a6683a0d40d792f3e1
MSokolov <maxim.sokolov@siriusxm.com>
2022-10-31 19:09:12 +0300

    Remove spoilers

--- src/main/kotlin/UsersManagementApp.kt
+++ src/main/kotlin/UsersManagementApp.kt
@@ -1,15 +1,2 @@
-import io.dropwizard.Application
-import io.dropwizard.setup.Environment
-
-class UsersManagementApp : Application<UserManagementConfiguration>() {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            UsersManagementApp().run()
-        }
-    }
-
-    override fun run(configuration: UserManagementConfiguration?, environment: Environment?) {
-        TODO("Not yet implemented")
-    }
+class UsersManagementApp() {
 }
